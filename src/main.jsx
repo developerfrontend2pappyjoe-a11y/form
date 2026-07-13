5804587d205a8865822456076997507995edb3f2
Albin <developerbackend2.pappyjoe@gmail.com>
2026-07-13 12:23:11 +0530

    Auto-save: 7/13/2026, 12:23:11 PM

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import InfoDetails from "./infodetails.js";
+import InfoDetails from "./infodetails.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
