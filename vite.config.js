8f7836e32475d8d805a523668cd334a75be48423
Albin <developerbackend2.pappyjoe@gmail.com>
2026-07-13 12:08:35 +0530

    Auto-save: 7/13/2026, 12:08:35 PM

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({ include: /\.(jsx|js|tsx|ts)$/ })],
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js$/,
-  },
+  plugins: [react()],
 })
